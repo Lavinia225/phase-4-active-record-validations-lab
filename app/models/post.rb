e99a9cfd6df74ff3a1129a5a6f81a507d2c7c2e0
Luna Seifert <thepuddingdeity@gmail.com>
2023-06-11 14:51:27 -0500

    commit tree 8fd094fce7e030b2cbb6cab601a1cf646e74a6a4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_clickbait?
+
+    def title_clickbait?
+        clickbait = [ /Won't Believe/i,/Secret/i,/Top \d/i,/Guess/i]
+
+        if clickbait.none? {|bait| bait.match(title)}
+            errors.add(:title, "Must be clickbait")
+        end
+    end
 end
